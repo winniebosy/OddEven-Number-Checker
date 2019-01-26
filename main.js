3072cdbaa7f0927d47565c6210cf64d1654ba8e5
winnie bosy <winniebosy@gmail.com>
2019-01-26 21:50:22 +0300

    made changes across the app file

--- main.js
+++ main.js
@@ -5,25 +5,29 @@ function getNumbers() {
     var num = '';
     num = document.getElementById("number").value;
 
-    if (num % 2 !==""){
-    document.getElementById("output").innerHTML = num + " " + "Kindly enter a number";
-    }
-    if (num % 2 === 0) {
+    if ((num % 2) !==0){
+    document.getElementById("output").innerHTML = num + " " + "is odd!";
+    }else if (num % 2 === 0) {
         document.getElementById("output").innerHTML = num + " " + "is even!";
     }
     else {
-        document.getElementById("output").innerHTML = num + " " + "is odd!";
+        
+        document.getElementById("output").innerHTML = num + " " + "Kindly enter a number";
+    }
 
 
         
     }
     //this fucntion will allow only number to be entered in the text box
-    function allowOnlyNumbers(evt) {
+    function checkIt(evt) {
         evt = (evt) ? evt : Window.event;
         var charCode = (evt.which) ? evt.which : evt.keyCode;
         if (CharCode > 31 && (charCode < 48 || charCode > 57)) {
+            status ="This field accepts numbers only "
             return false;
         }
+
+        status =" "
         return true;
     }
-}
+
