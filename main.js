d5a5b1d0667fbece9518d30c88d06601bf1d03a8
winnie bosy <winniebosy@gmail.com>
2019-01-26 18:08:01 +0300

    made changes to the three files

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
+
+
+//
 function getNumbers() {
     var num = '';
     num = document.getElementById("number").value;
+
+    if (num % 2 !==""){
+    document.getElementById("output").innerHTML = num + " " + "Kindly enter a number";
+    }
     if (num % 2 === 0) {
         document.getElementById("output").innerHTML = num + " " + "is even!";
     }
     else {
         document.getElementById("output").innerHTML = num + " " + "is odd!";
+
+
+        
     }
     //this fucntion will allow only number to be entered in the text box
     function allowOnlyNumbers(evt) {
